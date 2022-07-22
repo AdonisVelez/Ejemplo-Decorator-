b9060562e36e2ad7b7bb19b382f5c7c5960da952
AdonisVelez <106783832+AdonisVelez@users.noreply.github.com>
2022-07-21 23:22:42 -0500

    Update OrdenPrimium.cs

--- OrdenPrimium.cs
+++ OrdenPrimium.cs
@@ -4,7 +4,7 @@ public class OrdenPrimium : Decorador
 
     public override double CalcularTotalPrecioOrden()
     {
-        Console.WriteLine("Calculo del precio total en la clase{nameof(OrdenPrimium)}");
+        Console.WriteLine($"Calculo del precio total en la clase{nameof(OrdenPrimium)}");
         var preordenprecio = base.CalcularTotalPrecioOrden();
         Console.WriteLine("Adicion de un descuento adicional al precio anticipado");
         return preordenprecio*0.9;
